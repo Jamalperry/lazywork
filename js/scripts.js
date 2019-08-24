79f3c58d4a70377a8c552ad8cb7b5c29633aac08
Jamalperry <lee.perry400@gmail.com>
2019-08-24 14:54:27 -0400

    add initial changes and editing

---
+++ js/scripts.js
@@ -0,0 +1,56 @@
+function myAnts() {
+	document.getElementById("fact1").innerHTML = "They are unteachable, careless and indifferent.";
+}
+
+function myVerse1() {
+	document.getElementById("verse1").innerHTML = "P";
+}
+
+function myAnts2() {
+	document.getElementById("fact2").innerHTML = "They love excessive sleep.";
+}
+
+function myVerse2() {
+	document.getElementById("verse2").innerHTML = "T.";
+}
+
+function myAnts3() {
+	document.getElementById("fact3").innerHTML = "Irritating to others.";
+}
+
+function myVerse3() {
+	document.getElementById("verse3").innerHTML = "#";
+}
+
+function myAnts4() {
+	document.getElementById("fact4").innerHTML = "Servants to others.";
+}
+
+function myVerse4() {
+	document.getElementById("verse4").innerHTML = "They are unteachable, careless and indifferent.";
+}
+
+function myAnts5() {
+	document.getElementById("fact5").innerHTML = "Too lazy to cook.";
+}
+
+function myVerse5() {
+	document.getElementById("verse5").innerHTML = "T";
+}
+
+function myAnts6() {
+	document.getElementById("fact6").innerHTML = "Wishful thinkers.";
+}
+
+function myVerse6() {
+	document.getElementById("verse6").innerHTML = "t";
+}
+
+function myAnts#() {
+	document.getElementById("fact#").innerHTML = "#";
+}
+
+function myVerse#() {
+	document.getElementById("verse#").innerHTML = "#";
+}
+
