ff188767171c0f544a407596833d746218c3f5a0
Jamalperry <lee.perry400@gmail.com>
2020-01-20 12:04:47 -0400

    add more modifications

--- js/scripts.js
+++ js/scripts.js
@@ -1,51 +1,21 @@
 $(document).ready(function() {
 	$(".slug1").click(function() {
-		$(".img1").animate( {
-			right: '100px',
-			width: '180px',
-			height: '180px'
-			});
-		$(".sloth1").toggle();
+		$(".sloth1").slideToggle();
 		});
 	$(".slug2").click(function() {
-		$(".img2").animate( {
-			left: '100px',
-			width: '180px',
-			height: '180px'
-			});
-		$(".sloth2").toggle();
+		$(".sloth2").slideToggle();
 	});
 	$(".slug3").click(function() {
-		$(".img3").animate( {
-			right: '150px',
-			width: '180px',
-			height: '180px'
-			});
-		$(".sloth3").toggle();
+		$(".sloth3").slideToggle();
 	});
 	$(".slug4").click(function() {
-		$(".img4").animate( {
-			left: '150px',
-			width: '180px',
-			height: '180px'
-			});
-		$(".sloth4").toggle(1000);
+		$(".sloth4").slideToggle(1000);
 	});
 	$(".slug5").click(function() {
-		$(".img5").animate( {
-			right: '150px',
-			width: '180px',
-			height: '180px'
-			});
-		$(".sloth5").toggle();
+		$(".sloth5").slideToggle();
 	});
 	$(".slug6").click(function() {
-		$(".img6").animate( {
-			right: '150px',
-			width: '180px',
-			height: '180px'
-			});
-		$(".sloth6").toggle();
+		$(".sloth6").slideToggle();
 	});
 			
 });
